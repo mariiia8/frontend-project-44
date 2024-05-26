1bad2c5d91a6cf63c54c069375641cecb9a33355
mariiia8 <mariasidorova567@gmail.com>
2024-05-26 12:16:20 +0300

    fix import

--- src/gameBody.js
+++ src/gameBody.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-import greeting from '../cli.js';
+import greeting from './cli.js';
 
 const gameBody = (description, taskFunc) => {
   const playerName = greeting();
